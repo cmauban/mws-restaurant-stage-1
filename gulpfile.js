99af8decca83a524669ae4b5440750c09f84020f
Camille Mauban <cm376@duke.edu>
2018-07-17 16:48:38 -0400

    chore: added jasmine and dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,16 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
+var jasmine = require('gulp-jasmine-phantom');
 
 // Static server browser-sync
-gulp.task('default', ['sass'], function() {
+gulp.task('default', ['copy-html', 'copy-images', 'sass'], function() {
     gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('/index.html', ['copy-html']);
 
     browserSync.init({
         server: {
-            server: "./"
+            server: "./dist"
         }
     });
 });
@@ -25,10 +27,30 @@ gulp.task('sass', function() {
         .pipe(autoprefixer({
           browsers: ['last 2 versions']
         }))
-        .pipe(gulp.dest('./css'))
-        .pipe(browserSync.strem());
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
 });
 
+// Copy index.html to dist folder
+gulp.task('copy-html', function() {
+  gulp.src('./index.html')
+    .pipe(gulp.dest('./dist'));
+});
+
+// Copy images to dist folder
+gulp.task('copy-images', function() {
+  gulp.src('img/*')
+    .pipe(gulp.dest('dist/img'));
+});
+
+// TODO: add tests and make sure function work
+gulp.task('test', function() {
+  gulp.src('tests/spec/extraSpec.js')
+    .pipe(jasmine({
+      integration: true,
+      vendor: 'js/*.js'
+    }));
+})
 // Concatenate & Minify JS
 // gulp.task('scripts', function() {
 //     return gulp.src('js/*.js')
